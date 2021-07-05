350b46c074c6e0254cdf045839c89ce0bc587a7b
DikaMuharman <dika1254@gmail.com>
2021-07-05 14:45:21 +0700

    menambahkan halaman crud wisata

---
+++ resources/views/admin/wisata/index.blade.php
@@ -0,0 +1,101 @@
+@extends('layouts.admin')
+
+@section('title','Daftar wisata')
+
+@section('content')
+<div class="card shadow mb-4">
+  <div class="card-body">
+    @if (session('status'))
+    <div class="alert alert-success alert-dismissible fade show" role="alert">
+      <span>{{session('status')}}</span>
+      <button type="button" class="close" data-dismiss="alert" aria-label="Close">
+        <span aria-hidden="true">&times;</span>
+      </button>
+    </div>
+    @endif
+    <div class="d-flex flex-row justify-content-between align-items-center mb-4">
+      <h2 class="h4"><strong>List wisata</strong></h2>
+      <a href="{{route('wisata.create')}}" class="btn btn-primary">Tambah wisata</a>
+    </div>
+    <div class="table-responsive">
+      <table class="table table-hover">
+        <thead>
+          <tr>
+            <th scope="col">No</th>
+            <th scope="col">Nama Wisata</th>
+            <th scope="col">Status</th>
+            <th scope="col">Action</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <th scope="row">1</th>
+            <td>Singapura</td>
+            <td>Aktif</td>
+            <td>
+              <button class="btn btn-primary"><i class="far fa-eye"></i></button>
+              <a href="#" class="btn btn-warning"><i class="fas fa-edit"></i></a>
+              <a href="#" class="btn btn-danger"><i class="far fa-trash-alt"></i></a>
+            </td>
+          </tr>
+        </tbody>
+      </table>
+    </div>
+  </div>
+</div>
+
+{{-- Modal detail --}}
+<div class="modal fade" id="detailUser" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+  <div class="modal-dialog modal-lg">
+    <div class="modal-content">
+      <div class="modal-header">
+        <h5 class="modal-title" id="exampleModalLabel">Detail pengguna</h5>
+        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+          <span aria-hidden="true">&times;</span>
+        </button>
+      </div>
+      <div class="modal-body">
+        <div class="form-group">
+            <label for="username">Username</label>
+            <input type="text" name="username" class="form-control" readonly>
+        </div>
+        <div class="form-group">
+            <label for="email">Email</label>
+            <input type="text" name="email" class="form-control" readonly >
+        </div>
+        <div class="form-group">
+            <label for="role">Role</label>
+            <input type="text" name="role" class="form-control" readonly >
+        </div>
+      </div>
+      <div class="modal-footer">
+        <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+        <a class="btn btn-primary link">Edit</a>
+      </div>
+    </div>
+  </div>
+</div>
+@endsection
+
+@section('js')
+    <script>
+      $('#detailUser').on('show.bs.modal', function (event) {
+        var button = $(event.relatedTarget)
+        var url = button.data('url')
+        $.ajax({
+          url: url,
+          type: 'GET',
+          dataType: 'html'
+        }).done(function(data) {
+          var dataJson = $.parseJSON(data)
+          console.log(dataJson)
+          $('input[name="username"]').val(dataJson.username)
+          $('input[name="email"]').val(dataJson.email)
+          $('input[name="role"]').val(dataJson.role)
+          $('a.link').on('click',function() {
+            window.location.href = button.data('href')
+          })
+        })
+      });
+    </script>
+@endsection
---
+++ resources/views/admin/wisata/tambah.blade.php
@@ -0,0 +1,64 @@
+@extends('layouts.admin')
+
+@section('title','Tambah wisata')
+
+@section('content')
+    <div class="card">
+      <div class="card-body">
+          <form action="{{route('wisata.store')}}" method="POST">
+            @csrf
+            <div class="form-group">
+                <label for="nama">Nama</label>
+                <input type="text" name="nama" class="form-control @error('nama') is-invalid @enderror" value="{{old('nama')}}">
+                @error('nama')
+                    <span class="invalid-feedback">
+                        {{$message}}
+                    </span>
+                @enderror
+            </div>
+            <div class="form-group">
+                <label for="lokasi">Lokasi</label>
+                <input type="text" name="lokasi" class="form-control @error('lokasi') is-invalid @enderror" value="{{old('lokasi')}}">
+                @error('lokasi')
+                    <span class="invalid-feedback">
+                        {{$message}}
+                    </span>
+                @enderror
+            </div>
+            <div class="form-group">
+                <label for="deskripsi">Deskripsi</label>
+                <textarea name="deskripsi" class="form-control @error('deskripsi') is-invalid @enderror" >{{old('deskripsi')}}</textarea>
+                @error('deskripsi')
+                    <span class="invalid-feedback">
+                        {{$message}}
+                    </span>
+                @enderror
+            </div>
+            <div class="form-group">
+                <label for="foto">Foto</label>
+                
+                <div id="container" >
+                    <div class="input-group" >
+                        <div class="custom-file">
+                            <input type="file" class="custom-file-input" name="foto[]" id="photo" >
+                            <label class="custom-file-label" for="foto" >Choose file</label>
+                        </div>
+                        <div class="input-group-append">
+                            <button class="btn btn-danger" type="button" >Delete</button>
+                        </div>
+                    </div>
+                </div>
+                <button class="btn btn-primary mt-2" id="tambahGambar">Tambah Gambar</button>
+            </div>
+            <div class="d-flex justify-content-end ">
+                <a href="{{route('user.index')}}" class="btn btn-secondary mr-3">Back</a>
+                <button type="submit" class="btn btn-success">Submit</button>
+            </div>
+        </form>
+      </div>
+    </div>
+@endsection
+
+@section('js')
+    <script src="{{asset('js/photo.js')}}"></script>
+@endsection
